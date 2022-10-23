09015b6c9a8ef56f280ca002e869d370717cef53
eLSchiffer <eliska@sifner.cz>
2022-10-23 18:07:18 +0200

    As task forom L5

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-const podcast1 = {
-  num: '126',
-  title: 'Robot, který snědl koblihu',
-  guest: 'Radovan Siwek',
+import { Podcast } from './Podcast/Podcast.js';
+
+const episodes = [
+  {
+    num: 127,
+    title: 'Hledání plyšového Yettiho',
+    guest: 'Vojtěch Ryba',
+  },
+  {
+    num: 128,
+    title: 'Moderní hrachová polévka',
+    guest: 'Kamila Štancová',
+  },
+  {
+    num: 129,
+    title: 'Poloautomatické zrcadlo',
+    guest: 'Janka Janovská',
+  },
+  {
+    num: 130,
+    title: 'Máčené hlavy parlamentu',
+    guest: 'Jonáš Daněk',
+  },
+  {
+    num: 131,
+    title: 'Služby na hraně přívěsu',
+    guest: 'Tereza Křivánková',
+  },
+  {
+    num: 132,
+    title: 'Klasifikace sněžných klokanů',
+    guest: 'Josef Stix',
+  },
+  {
+    num: 133,
+    title: 'Rybolov v Oceánu bouří',
+    guest: 'Ludmila Gajová',
+  },
+];
+
+
+
+const renderEpisodes = () => {
+  const episodeList = document.querySelector('.episodes-list');
+  episodeList.innerHTML = episodes.map((episode) => Podcast(episode)).join('');
 };
 
+renderEpisodes(episodes);
 console.log('funguju!', podcast1);
